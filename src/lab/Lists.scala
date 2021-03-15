444459cc097d3d5f2050bbc566bbb2a23d8a77ee
ndido98 <ndido98@gmail.com>
2021-03-15 11:15:01 +0100

    Prepare Lists for lab exercises

---
+++ src/lab/Lists.scala
@@ -0,0 +1,34 @@
+package lab
+
+object Lists {
+
+    // A generic linked list
+    sealed trait List[E]
+
+    // a companion object (i.e., module) for List
+    object List {
+        case class Cons[E](head: E, tail: List[E]) extends List[E]
+        case class Nil[E]() extends List[E]
+
+        def sum(l: List[Int]): Int = l match {
+            case Cons(h, t) => h + sum(t)
+            case _ => 0
+        }
+
+        def append[A](l1: List[A], l2: List[A]): List[A] = (l1, l2) match {
+            case (Cons(h, t), l2) => Cons(h, append(t, l2))
+            case _ => l2
+        }
+
+        def map[A,B](l: List[A])(mapper: A=>B): List[B] = l match {
+            case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
+            case Nil() => Nil()
+        }
+
+        def filter[A](l1: List[A])(pred: A=>Boolean): List[A] = l1 match {
+            case Cons(h,t) if (pred(h)) => Cons(h, filter(t)(pred))
+            case Cons(_,t) => filter(t)(pred)
+            case Nil() => Nil()
+        }
+    }
+}
---
+++ test/lab/ListsTests.scala
@@ -0,0 +1,35 @@
+package lab
+
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+
+object ListsTests {
+    import Lists.List._
+
+    val testList = Cons(1, Cons(2, Cons(3, Nil())))
+
+    @Test
+    def testSum() = {
+        assertEquals(6, sum(testList))
+        assertEquals(0, sum(Nil()))
+    }
+
+    @Test
+    def testAppend() = {
+        val appendedList = Cons(1, Cons(2, Cons(3, Cons(4, Nil()))))
+        assertEquals(appendedList, append(testList, Cons(4, Nil())))
+    }
+
+    @Test
+    def testMap() = {
+        val mapped = Cons(10, Cons(20, Cons(30, Nil())))
+        assertEquals(mapped, map(testList)(_ * 10))
+        assertEquals(Nil[Int](), map(Nil[Int]())(x => x))
+    }
+
+    @Test
+    def testFilter() = {
+        val odd = Cons(1, Cons(3, Nil()))
+        assertEquals(odd, filter(testList)(_ % 2 == 1))
+    }
+}
